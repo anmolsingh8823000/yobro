95f578da447f7399a09da9e6b330efd47bf39e88
Divy Jain <divykjain@gmail.com>
2016-11-19 14:47:47 +0530

    Added Greetings Detection

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Installing YoBro..."
-echo "alias yobro=\"~/yobro/main.py\"" >> ~/.bashrc && source ~/.bashrc
-echo "YoBro in the house! ;)"
--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-#!/usr/bin/python
-print ("Main Started")
-print ("Yo Bro! What's up?")
+#!/usr/bin/python3
+
+import sys
+from imports import prefRandOut
+
+input = ""
+
+for arg in sys.argv[1:]:
+	input = input + " " +arg
+
+input = input.lower()
+
+def greet():
+	print("Hey! This is YoBro")
+
+greetings = ['hi','hello','hey','heya']
+
+for greets in greetings:
+	if input.find(greets):
+		greet()
+		break
